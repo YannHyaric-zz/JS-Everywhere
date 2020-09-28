3bb6ec79f8e7cad34ae94117732abad763677226
YannHyaric <ylhalmeida@gmail.com>
2020-09-28 09:51:23 -0300

    resolver

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,43 +1,28 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 require('dotenv').config();
+
+// Local module imports
 const db = require('./db');
 const models = require('./models');
 const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
 
-// Run the server on a port specified in our .env file or port 4000
+// Run our server on a port specified in our .env file or port 4000
 const port = process.env.PORT || 4000;
-
-// Store the DB_HOST value as a variable
 const DB_HOST = process.env.DB_HOST;
-
-// Provide resolver functions for our schema fields
-const resolvers = {
-  Query: {
-    hello: () => 'Hello world!',
-    notes: async () => {
-      return await models.Note.find();
-    },
-    note: async (parent, args) => {
-      return await models.Note.findById(args.id);
-    }
-  },
-
-  Mutation: {
-    newNote: async (parent, args) => {
-      return await models.Note.create({
-        content: args.content,
-        author: 'Adam Scott'
-      });
-    }
-  }
-};
-
 const app = express();
 db.connect(DB_HOST);
 
 // Apollo Server setup
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: () => {
+    // Add the db models to the context
+    return { models };
+  }
+});
 
 // Apply the Apollo GraphQL middleware and set the path to /api
 server.applyMiddleware({ app, path: '/api' });
