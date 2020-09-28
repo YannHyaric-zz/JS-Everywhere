722fbf5be15c12b84921e6eb636a78d2df3cdf43
YannHyaric <ylhalmeida@gmail.com>
2020-09-28 16:35:51 -0300

    autentication

--- backend/src/resolvers/query.js
+++ backend/src/resolvers/query.js
@@ -1,8 +1,8 @@
 module.exports = {
   notes: async (parent, args, { models }) => {
-  return await models.Note.find()
+    return await models.Note.find();
   },
   note: async (parent, args, { models }) => {
-  return await models.Note.findById(args.id);
+    return await models.Note.findById(args.id);
   }
-  }
+};
